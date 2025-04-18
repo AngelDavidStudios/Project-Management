645711be99125ed9d2790d71d60d2b72f06be386
Angel David <angeldavidstudios@outlook.com>
2025-04-18 17:34:05 -0500

    Implementacion de types hacia los modelos de datos del backend

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface Task {
+  id: string;
+  titulo: string;
+  descripcion: string;
+  fechaAsignada: string;
+  fechaLimite: string;
+  estado: number; // 0: Pendiente, 1: En progreso, 2: Completada, 3: Retrasada
+}
+
+export interface Project {
+  id: string;
+  nombre: string;
+  descripcion: string;
+  fechaInicio: string;
+  fechaFin: string;
+  empleadosAsignados: string[];
+  tareas: Task[];
+}
+
+export interface Employee {
+  id: string;
+  nombre: string;
+  correo: string;
+  proyectosAsignados: string[];
+}
+
+export interface DateRange {
+  startDate: string;
+  endDate: string;
+}
